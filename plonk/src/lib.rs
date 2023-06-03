d80cf8e226c36bf1dad879beb6ff4244c170c94f
James Snewin <james.snewin@immutable.com>
2023-06-03 13:51:53 +1000

    mapping gates

--- plonk/src/lib.rs
+++ plonk/src/lib.rs
@@ -29,10 +29,20 @@
 
 // evaluation:
 
-#[derive(Debug)]
-struct Gate(i32, i32, i32, i32, i32);
+use std::{
+    collections::{HashMap, HashSet},
+    f32::MIN, fmt::format,
+};
 
-impl Gate {
+const PROGRAM: &str = "e * x + x - 1";
+const ADD: char = '+';
+const MINUS: char = '-';
+const MUL: char = '*';
+
+#[derive(Debug)]
+struct QGate(i32, i32, i32, i32, i32);
+
+impl QGate {
     fn new_mul() -> Self {
         Self(0, 0, 1, -1, 0)
     }
@@ -43,43 +53,105 @@ impl Gate {
 }
 
 #[derive(Debug)]
+enum GateType {
+    ADDITION,
+    MUL,
+}
+
+#[derive(Debug)]
+struct Gate {
+    gate: (String, String, String),
+    gateType: GateType,
+}
+
+impl Gate {
+    fn new_gates(program: &str) -> Vec<Self> {
+        let mut v_matrix: Vec<Gate> = Vec::new();
+        // replace all spaces
+        let program = program.replace(" ", "");
+        let chars: Vec<char> = program.chars().collect();
+
+        // itterative over all character, where an operator is found, a new gate is created
+        for i in 0..chars.len() {
+            if chars[i] == ADD || chars[i] == MINUS || chars[i] == MUL {
+                // build l, r, o nodes
+                let mut l;
+                let r = chars[i + 1].to_string();
+                let o = format!("GO:{}", v_matrix.len());
+
+                if v_matrix.len() == 0 {
+                    // if matrix is empty, l is the first character
+                    l = chars[i - 1].to_string();
+                } else {
+                    // else l is the output of the previous gate
+                    l = v_matrix[v_matrix.len()-1].gate.2.clone();
+                }
+
+
+                match chars[i] {
+                    MUL => v_matrix.push(Gate {
+                        gate: (l, r, o),
+                        gateType: GateType::MUL,
+                    }),
+                    _ => v_matrix.push(Gate {
+                        gate: (l, r, o),
+                        gateType: GateType::ADDITION,
+                    }),
+                }
+            }
+        }
+        v_matrix
+    }
+
+}
+
+#[derive(Debug)]
 struct Trace(i32, i32, i32);
 
-impl Trace {
-    const ADD:  &str = "+";
-    const MINUS: &str = "-";
-    const MUL:   &str = "*";
+impl Trace {}
 
-    fn new(program : &str) -> Self {
-        
-    }
-
-    fn new_trace(program : &str) -> Vec<Self> {
-        
-    }
-}
-
-fn eval_q_row(gate :Gate, trace : Trace) -> i32 {
+fn eval_q_row(gate: QGate, trace: Trace) -> i32 {
     // Ai(QL)i + Bi(QR)i + AiBiQm + Ci(QO)i + QCi
-    trace.0*gate.0 + trace.1*gate.1 + trace.0*trace.1*gate.2 + trace.2*gate.3 + gate.4
+    trace.0 * gate.0 + trace.1 * gate.1 + trace.0 * trace.1 * gate.2 + trace.2 * gate.3 + gate.4
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn eval_add_gate() {
-        let gate = Gate::new_add();
+    fn test_eval_add_gate() {
+        let gate = QGate::new_add();
         let trace = Trace(1, 2, 3);
         assert_eq!(eval_q_row(gate, trace), 0);
     }
 
     #[test]
-    fn eval_mul_gate() {
-        let gate = Gate::new_mul();
+    fn test_eval_mul_gate() {
+        let gate = QGate::new_mul();
         let trace = Trace(2, 5, 10);
         assert_eq!(eval_q_row(gate, trace), 0);
+    }
+
+    #[test]
+    fn test_new_gates() {
+        let expected_gates: Vec<Gate> = vec![
+            Gate {
+                gate: ("e".to_string(), "x".to_string(), "GO:0".to_string()),
+                gateType: GateType::MUL,
+            },
+            Gate {
+                gate: ("GO:0".to_string(), "x".to_string(), "GO:1".to_string()),
+                gateType: GateType::ADDITION,
+            },
+            Gate {
+                gate: ("GO:1".to_string(), "1".to_string(), "GO:2".to_string()),
+                gateType: GateType::ADDITION,
+            },
+        ];
+
+        assert_eq!(expected_gates[0].gate, Gate::new_gates(PROGRAM)[0].gate);
+        assert_eq!(expected_gates[1].gate, Gate::new_gates(PROGRAM)[1].gate);
+        assert_eq!(expected_gates[2].gate, Gate::new_gates(PROGRAM)[2].gate);
     }
 }
